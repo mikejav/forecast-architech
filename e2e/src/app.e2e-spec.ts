e8e92a9cd2229f601f1db97d329ed5d6491d8148
Michal Jaworski <mikejav@users.noreply.github.com>
2019-09-27 22:40:05 +0200

    update tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to forecast-archtech!');
+    expect(page.getTitleText()).toEqual('Welcome to ng-weather-app!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'forecast-archtech'`, () => {
+  it(`should have as title 'ng-weather-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('forecast-archtech');
+    expect(app.title).toEqual('ng-weather-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to forecast-archtech!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-weather-app!');
   });
 });
